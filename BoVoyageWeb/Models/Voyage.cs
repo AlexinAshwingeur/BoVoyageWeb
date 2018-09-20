fcccc97e2024bad10a61d2f4f912ea541e7e8687
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 10:32:02 +0200

    Ajout de la classe Voyage aves les paramètres et les relations

--- BoVoyageWeb/Models/Voyage.cs
+++ BoVoyageWeb/Models/Voyage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,26 @@ namespace BoVoyageWeb.Models
 {
     public class Voyage
     {
+        public int Id { get; set; }
+
+        public DateTime DateAller { get; set; }
+
+        public DateTime DateRetour { get; set; }
+
+        public int PlacesDisponibles { get; set; }
+
+        public decimal PrixParPersonne { get; set; }
+
+        //public List<DossierReservation> Dossiers { get; set; }
+
+        public int IdAgenceVoyage { get; set; }
+        [ForeignKey("IdAgenceVoyage")]
+        public virtual AgenceVoyage AgenceVoyage { get; set; }
+
+        public int IdDestination { get; set; }
+        [ForeignKey("IdDestination")]
+        public virtual Destination Destination { get; set; }
+
 
     }
 }
