3c70a2dcf98d41527ef58d55c476fb55907f0407
ashwingeur <january05@hotmail.fr>
2018-09-20 11:06:03 +0200

    Creation de notre classe DbContext

---
+++ BoVoyageWeb/Data/BoVoyageDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageWeb.Data
+{
+    public class BoVoyageDbContext : DbContext
+    {
+        public BoVoyageDbContext() : base("BoVoyageWebConnectionString")
+        {
+        }
+
+
+    }
+}
