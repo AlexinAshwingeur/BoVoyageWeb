9b6729b67b9d15f8976533870d5079665dc0f4b8
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 10:52:43 +0200

    Ajout de Required et de StringLenght pour le Nom dans la classe Personne

--- BoVoyageWeb/Models/Personne.cs
+++ BoVoyageWeb/Models/Personne.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,11 +10,19 @@ namespace BoVoyageWeb.Models
     public class Personne
     {
         public int Id { get; set; }
+
         public string Civilite { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string Nom { get; set; }
+
         public string Prenom { get; set; }
+
         public string Adresse { get; set; }
+
         public string Telephone { get; set; }
+
         public DateTime DateNaissance { get; set; }
     }
 }
