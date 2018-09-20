552d20893febe134f79e0e6b5f108ebff3bffd28
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 12:44:35 +0200

    Mapping dans la base de données en Table Clients

--- BoVoyageWeb/Models/Client.cs
+++ BoVoyageWeb/Models/Client.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyageWeb.Models
 {
+    [Table("Clients")]
     public class Client : Personne
     {
 
