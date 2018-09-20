71a066dd42f5a9c3ad5063346ce112dc9a2d9c37
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 10:09:53 +0200

    Ajout de using dans la classe AgenceVoyage

--- BoVoyageWeb/Models/AgenceVoyage.cs
+++ BoVoyageWeb/Models/AgenceVoyage.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+
 namespace BoVoyageWeb.Models
 {
+    [Table("AgencesVoyages")]
     public class AgenceVoyage
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nom { get; set; }
+
+        public List<Voyage> Voyages { get; set; }
+
     }
 }
