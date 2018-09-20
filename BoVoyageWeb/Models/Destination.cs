f65d62515ee9f63998b227f8bfa2320b285497c4
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 12:45:18 +0200

    Mapping dans la base de données en Table Destinations

--- BoVoyageWeb/Models/Destination.cs
+++ BoVoyageWeb/Models/Destination.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace BoVoyageWeb.Models
 {
+    [Table("Destinations")]
     public class Destination
     {
         public int Id { get; set; }
