39d0c3dadb4675966adb28ad2c0733702c269e47
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 10:51:13 +0200

    Ajout de la classe Dossier Reservation

---
+++ BoVoyageWeb/Models/DossierReservation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageWeb.Models
+{
+    [Table("DossiersReservations")]
+    public class DossierReservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string NumeroCarteBancaire { get; set; }
+
+        public decimal PrixParPersonne { get; set; }
+
+        [NotMapped]
+        public decimal PrixTotal { get; set; }
+
+        public int IdVoyage { get; set; }
+        [ForeignKey("IdVoyage")]
+        public Voyage Voyage { get; set; }
+
+        public int IdClient { get; set; }
+        [ForeignKey("IdClient")]
+        public Client Client { get; set; }
+
+        [NotMapped]
+        public byte EtatDossierReservation { get; set; }
+
+        public ICollection<Assurance> Assurances { get; set; }
+
+        public  ICollection<Participant> ListParticipants { get; set; }
+    }
+}
