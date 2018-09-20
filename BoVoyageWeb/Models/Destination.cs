db2d1e0e7294d44f5e1bb95eae963a1a2c46b6e4
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 10:51:42 +0200

    Ajout de StringLength sur Description de la classe Destination

--- BoVoyageWeb/Models/Destination.cs
+++ BoVoyageWeb/Models/Destination.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -15,6 +16,7 @@ namespace BoVoyageWeb.Models
 
         public string Region { get; set; }
 
+        [StringLength(250)]
         public string Description { get; set; }
 
         public List<Voyage> Voyages { get; set; }
