6815a5fa8422ba39eaa4c3656b060f6b35bd4882
AlexinaO <alexina.orsoni@gmail.com>
2018-09-20 10:31:30 +0200

    Ajout de la classe Destintation avec les paramètres et les relations

---
+++ BoVoyageWeb/Models/Destination.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageWeb.Models
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+
+        public string Continent { get; set; }
+
+        public string Pays { get; set; }
+
+        public string Region { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Voyage> Voyages { get; set; }
+
+    }
+}
